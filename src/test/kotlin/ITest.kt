5c1c6d555fa288ac6047dd8ef20ad279a8ad6f32
frodank <11638682+frodank@users.noreply.github.com>
2023-03-21 10:57:30 +0100

    Integrasjons-test trenger også alfabetisk sortering av graf-instrukser

--- src/test/kotlin/ITest.kt
+++ src/test/kotlin/ITest.kt
@@ -31,12 +31,12 @@ class ITest {
             ```mermaid
             graph TD;
             rekrutteringsbistand-stilling-api;
-            toi-oppfolgingsinformasjon --> toi-identmapper;
-            toi-identmapper --> toi-sammenstille-kandidat;
             toi-arbeidsmarked-cv --> toi-sammenstille-kandidat;
+            toi-identmapper --> toi-sammenstille-kandidat;
+            toi-oppfolgingsinformasjon --> toi-identmapper;
+            toi-oppfolgingsperiode --> toi-sammenstille-kandidat;
             toi-sammenstille-kandidat --> toi-synlighetsmotor;
             toi-synlighetsmotor;
-            toi-oppfolgingsperiode --> toi-sammenstille-kandidat;
             ```
         """.trimIndent(), mermaids[0])
     }
